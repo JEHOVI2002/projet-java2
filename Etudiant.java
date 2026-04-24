35ea59c2fd8356f50be893a643bb93e2a59d64d7
Rimy-Mz <meziani.rimy2023@gmail.com>
2026-04-24 06:57:19 -0400

    Update Etudiant.java
    Ajout de commentaires

--- Etudiant.java
+++ Etudiant.java
@@ -1,24 +1,30 @@
-
+// Creation class Etudiant
 public class Etudiant {
     private String nom;
     private double[] notes;
     private double moyenne;
-#creation class etdudianthhggguhiijol
+
+// Constructeur: initialise le nom et les notes
+// Calcul de ma moyenne    
     public Etudiant(String nom, double[] notes) {
         this.nom = nom;
         this.notes = notes;
         calculerMoyenne();
     }
 
+// Methode pour Calcluc de la moyenne    
     public void calculerMoyenne() {
         double somme = 0;
+// Parcourt de notes et calcul de la somme        
         for (double note : notes) {
             somme += note;
         }
+// Calcul de moyenne
         this.moyenne = somme / notes.length;
     }
 
-    public double getMoyenne() {
+// Retourner la moyenne d'étudiant    
+        public double getMoyenne() {
         return moyenne;
     }
 
@@ -26,6 +32,7 @@ public class Etudiant {
         return nom;
     }
 
+// Redéfinition de la méthode toString pour afficher l'étudiant    
     @Override
     public String toString() {
         return nom + " - Moyenne: " + moyenne;
